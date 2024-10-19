f54ba0fa0491a8ee166864f18bd4f0a9d06320a0
lucas campos <lucascampos.mg@gmail.com>
2024-10-19 12:11:13 -0300

    add http config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,12 @@
+import ExpressServer from "./infra/http/express/ExpressServer"
+import path from "path";
+
+// Configurações servidor http
+const directoryPathForPublicImages = path.join(__dirname, "..", "public");
+const server = new ExpressServer(directoryPathForPublicImages);
+
+// TODO add rotas aqui
+
+server.listen(3000);
+
 console.log("es-call backend running");
