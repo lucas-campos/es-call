5ef887873aa6748b1f78ae3a262d1482eabce411
lucas campos <lucascampos.mg@gmail.com>
2024-10-28 19:36:33 -0300

    add api for return all support technician

--- backend/src/Routes.ts
+++ backend/src/Routes.ts
@@ -1,9 +1,11 @@
 import GetAllCallsController from "./controller/GetAllCallsController";
+import GetAllSupportTechnicianController from "./controller/GetAllSupportTechnicianController";
 import Server, { HttpMethod } from "./infra/http/Server";
 
 export default class Routes {
 
     static defineRoutes(server: Server) {
         server.on(HttpMethod.GET, "/call", new GetAllCallsController());
+        server.on(HttpMethod.GET, "/support-technician", new GetAllSupportTechnicianController())
     }
 }
--- backend/src/database_script/generate_schema.sql
+++ backend/src/database_script/generate_schema.sql
@@ -7,15 +7,15 @@ create table escall.support_technician (
 
 create table escall.call (
     id int primary key not null,
-    support_technician_id int not null,
+    support_technician_id int,
     name varchar (100) not null,
     phone_number varchar (100) not null,
     description varchar (200) not null,
     status varchar (100) not null,
     created_at timestamp not null,
-    technician_added_date timestamp not null,
-    finished_at timestamp not null,
-    priority varchar (100) not null,
+    technician_added_date timestamp,
+    finished_at timestamp,
+    priority varchar (100),
     constraint call_support_technician_fk foreign key (support_technician_id) references escall.support_technician(id)    
 );
 
